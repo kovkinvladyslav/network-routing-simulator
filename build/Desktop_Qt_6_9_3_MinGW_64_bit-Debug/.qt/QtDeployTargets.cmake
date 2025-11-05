57490748c676bf36ead038760f6eb33961542fbe
kovkinvladyslav <kovkinvlad@gmail.com>
2025-11-05 19:51:25 +0200

    remove gitignored files

--- build/Desktop_Qt_6_9_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_kpsrc_FILE C:/Users/admin/Desktop/prog/networks/kpsrc/build/Desktop_Qt_6_9_3_MinGW_64_bit-Debug/kpsrc.exe)
-set(__QT_DEPLOY_TARGET_kpsrc_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_kpsrc_RUNTIME_DLLS C:/Qt/6.9.3/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.3/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.3/mingw_64/bin/Qt6Core.dll)
